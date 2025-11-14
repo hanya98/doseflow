aafba71c5a47d7bafef4918241d3197901e8023b
hanya98 <hanya049bteceai25@igdtuw.ac.in>
2025-11-14 19:53:01 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVyJmknQtKryagVxwJ0Y0JzhJjXj4oXiE",
+    authDomain: "doseflow1.firebaseapp.com",
+    projectId: "doseflow1",
+    storageBucket: "doseflow1.firebasestorage.app",
+    messagingSenderId: "748085484992",
+    appId: "1:748085484992:web:1a6cd740ae4cd0c8863c25"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
